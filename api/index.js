97e58b504b678ef1c49e128deb7ca5662a40c144
josephenoch <enochjoseph01@gmail.com>
2022-07-12 00:19:27 +0100

    :wrench: remove boiler plate react app

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
 const cors = require('cors')
 const morgan = require('morgan')
 
@@ -10,3 +9,14 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan("tiny"))
 
+// require("./db/mongodb")
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Hello World</h1>`)
+  })
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`)
+})
